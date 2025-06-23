c53815e5e1e9b50ac1147624d073a899ce34b50b
Danilo Gonçalves Pinheiro <124277720+DaniloPin@users.noreply.github.com>
2025-06-23 17:16:16 -0300

    Add files via upload

---
+++ Funcionario.cs
@@ -0,0 +1,23 @@
+﻿public class Funcionario {
+   
+
+    public string Nome {  get; set; }
+    public string Cargo { get; set; }
+
+    public Funcionario(string nome, string cargo) {
+        Nome = nome;
+        Cargo = cargo;
+    }
+
+    public void Promover (string novoCargo) {
+        
+        if (Cargo != novoCargo) {
+           
+            Cargo = novoCargo;
+            Console.WriteLine("Promovido com sucesso!");
+        } 
+        else {
+            Console.WriteLine("O novo cargo deve ser diferente do atual.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Funcionario func = new Funcionario("Danilo Pinheiro", "Demitido");
+
+Console.WriteLine($"Nome do Funcionário: {func.Nome}");
+Console.WriteLine($"Cargo atual: {func.Cargo}\n");
+
+func.Promover("Maratonista");
+func.Promover("Judoca");
+
+Console.WriteLine("\n------ Após Promoção --------");
+
+Console.WriteLine($"\nNome do funcionário: {func.Nome}");
+Console.WriteLine($"Nova função: {func.Cargo}");
